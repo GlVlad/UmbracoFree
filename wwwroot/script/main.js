8ba6b17372dfba15d5363b784cd647a86ba44a6f
GlVlad <fostermoster15@gmail.com>
2023-12-29 14:10:49 +0200

    upload

---
+++ wwwroot/script/main.js
@@ -0,0 +1,13 @@
+$('.cab-btn').click(function(){
+    $('nav ul .cab-show').toggleClass("show");
+  });
+$('.pers-btn').click(function(){
+    $('nav ul ul .pers-show').toggleClass("show2");
+  });
+$('.task-btn').click(function(){
+    $('nav ul .task-show').toggleClass("show1");
+  });
+$('.kep-btn').click(function(){
+    $('nav ul ul .kep-show').toggleClass("show3");
+  });
+
